236f43060680f875b7225cde8b180ad03cf3e1a1
DGV98 <davidgonzalez1019@gmail.com>
2024-11-02 00:23:41 -0500

    Added hash tables

---
+++ hash_tables.py
@@ -0,0 +1,105 @@
+def first_non_repeated_char(str):
+    mapping = {}
+    for char in str:
+        if char in mapping:
+            mapping[char] += 1
+        else:
+            mapping[char] = 1
+    for char in str:
+        if mapping[char] == 1:
+            return char
+    return "None"
+
+
+def first_repeated_char(str):
+    s = set()
+    for char in str:
+        if char in s:
+            return char
+        s.add(char)
+    return ""
+
+
+def most_frequent_element(nums):
+    hashmap = {}
+    for num in nums:
+        if num in hashmap:
+            hashmap[num] += 1
+        else:
+            hashmap[num] = 1
+    max_el = None
+    for num, val in hashmap.items():
+        if max_el == None:
+            max_el = num
+        if val > hashmap[max_el]:
+            max_el = num
+    return max_el
+
+
+class Entry:
+    def __init__(self, key, value) -> None:
+        self.key = key
+        self.value = value
+
+
+class Node:
+    def __init__(self, entry: Entry):
+        self.entry = entry
+        self.next = None
+
+
+class HashTable:
+    def __init__(self, capacity):
+        self.map = [None] * capacity
+
+    def put(self, key, value):
+        entry = Entry(key, value)
+        node_entry = Node(entry)
+        index = self.hash_function(key)
+        if not self.map[index]:
+            self.map[index] = node_entry
+        else:
+            node = self.map[index]
+            while node.next:
+                node = node.next
+            node.next = node_entry
+
+    def get(self, key):
+        i = self.hash_function(key)
+        if not self.map[i]:
+            return None
+        node = self.map[i]
+        while node:
+            if node.entry.key == key:
+                return node.entry.value
+            node = node.next
+        return None
+
+    def remove(self, key):
+        i = self.hash_function(key)
+        if not self.map[i]:
+            return
+        node = self.map[i]
+        if node.entry.key == key:
+            self.map[i] = node.next
+        else:
+            prev = node
+            node = node.next
+            while node:
+                if node.entry.key == key:
+                    prev.next = node.next
+                prev = node
+                node = node.next
+
+    def hash_function(self, key):
+        capacity = len(self.map)
+        return key % capacity
+
+
+if __name__ == "__main__":
+    # print(first_non_repeated_char('green'))
+    # print(first_repeated_char('green'))
+    hashmap = HashTable(5)
+    hashmap.put(5, "Hello")
+    print(hashmap.get(5))
+    print(most_frequent_element([1, 1, 1, 2, 2, 4, 4, 4, 4, 5, 6, 7, 7, 7]))
